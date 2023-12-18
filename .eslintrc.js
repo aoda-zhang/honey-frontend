b2c1c3dce7cb74834894152ec6f2768fdb9de0d7
aodaGit <aoda2826@qq.com>
2023-12-18 22:26:53 +0800

    add code format

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports ={
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+},
+  ignorePatterns: [".eslintrc.js"],
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier"
+  ],
+  plugins: ["react", "react-hooks","@typescript-eslint","import"],
+  env: {
+    "browser": true,
+    "node": true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
+  rules: {
+    "no-var": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "react/jsx-uses-react": "warn",
+    "react/react-in-jsx-scope": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never'
+      }
+  ],
+  }
+}
